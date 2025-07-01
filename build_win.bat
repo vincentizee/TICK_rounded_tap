32da6d4dd9098b0d74cdaedc6f3cfc1b6ffb02a5
vincentizee <60829146+vincentizee@users.noreply.github.com>
2025-07-01 13:41:43 +0200

    Update build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -2,9 +2,4 @@
 echo Building GPL formats
 cmake -B ./build -DCOPY_AFTER_BUILD="FALSE" -DCMAKE_CXX_FLAGS="/DJUCE_DISPLAY_SPLASH_SCREEN=0"
 cmake --build ./build --clean-first --target TICK TICK_Standalone TICK_VST3 --config RelWithDebInfo
-echo Building non-GPL with Splash
-cmake -B ./build -DCOPY_AFTER_BUILD="FALSE" -DCMAKE_CXX_FLAGS="/DJUCE_DISPLAY_SPLASH_SCREEN=1"
-cmake --build ./build --target TICK TICK_VST TICK_AAX --config RelWithDebInfo
 
-@REM echo Sign AAX
-@REM python3 .\private\sign_aax.py ".\TICK_artefacts\RelWithDebInfo\AAX\TICK.aaxplugin\Contents\x64\TICK.aaxplugin"
